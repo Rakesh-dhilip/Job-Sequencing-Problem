902657b639029f4588dbe9cca9f3e55caf01d4bb
Rakesh-dhilip <83400020+Rakesh-dhilip@users.noreply.github.com>
2022-01-20 19:17:01 +0530

    Add files via upload

---
+++ JOB.java
@@ -0,0 +1,45 @@
+//Job Sequencing Problem
+public class JOB {
+
+    public static void main(String[] args) {
+
+        char name[]={'1','2','3','4'};
+        int dl[] = {1,1,4,1};
+        int profit[] ={40,30,20,10};
+
+        char cap[] = new char[2];
+        for (int i =0;i<2 ;i++)
+        {
+            cap[i]='\0';
+        }
+
+        int j;
+        int i =0;
+
+        j = dl[i]-1;
+
+        while (i<4)
+        {
+
+            if(j<0) {
+                i++;
+                if(i<4)
+                j = dl[i]-1;
+            }
+            else if(j<2 && cap[j]=='\0')
+            {
+                cap[j] = name[i];
+                i++;
+                if(i<4)
+                j = dl[i]-1;
+
+            }
+            else
+               j=j-1;
+
+        }
+
+        for (int i1 =0 ; i1< 2 ; i1++)
+            System.out.println(cap[i1]);
+    }
+}
